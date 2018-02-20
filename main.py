a7767d76dfe00445debfef8108c406a21d1257c3
Peter Cooper <pcooper19@gmail.com>
2018-02-20 11:01:16 -0500

    primary architecture

---
+++ main.py
@@ -0,0 +1,43 @@
+""" The idea of woof is simple: with the click of a button - you can send a customized messsage to a particular person
+across all of their communication platforms: twitter (DM), Instagram, Facebook (messeneger), a pop-up on their
+desktop/laptop, e-mail, phone voice message, github, slack and LinkedIn. This is essentially the 21st century human
+DDOS. This is meant to be used for people who really, really want to get in touch with another person. All you need to
+input is their contact information.
+
+"""
+
+# Figure out how to connect to twitter to send a DM to that person from our platform.
+
+
+# Figure out how to connect to Instagram to send a DM to that person from our platform.
+
+
+# Figure out how to connect to Facebook Messenger to send a DM to that person from our platform.
+
+
+# Figure out how to send a message to a particular persons laptop/computer screen as a pop-up message
+
+
+# Send an e-mail to the person
+
+
+# Send a voicemail to the persons cell phone
+
+
+# Figure out how to send a message to a particular persons Github
+
+
+# Figure out how to send a message to a particular persons Slack
+
+
+# Figure out how to send a message to a particular persons LinkedIn (Direct Message)
+
+
+
+
+
+#
+
+
+
+#
